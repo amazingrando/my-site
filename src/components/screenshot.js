7824c7f842ef6df09741e38cfde8dd433aa16b4f
Randy Oest <oest@amazingrando.com>
2024-04-15 22:29:15 -0400

    wip

---
+++ src/components/screenshot.js
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+import classNames from 'classnames';
+
+const ScreenshotPlayer = ({ title, src }) => (
+  <figure
+    className={classNames(
+      'p-2 pt-0 bg-slate-200 rounded',
+      'border border-slate-400 border-solid',
+      'lg:w-[150%]',
+    )}
+  >
+    <div className="!m-0 relative text-center">
+      <span className="tracking-[.2em] absolute left-0 top-0">🔴🟡🟢</span>
+      <span className="text-slate-700 font-bold text-sm uppercase">
+        {title}
+      </span>
+    </div>
+    <div className="max-h-[70dvh] overflow-y-scroll">
+      <Image src={src} alt={title} className="!m-0" />
+    </div>
+  </figure>
+);
+
+export default ScreenshotPlayer;
